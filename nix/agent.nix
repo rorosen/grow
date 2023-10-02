9df3805982c495c295d11807376cd6abd5c004c6
Robert Rose <robert.rose@mailbox.org>
2023-10-03 01:04:07 +0200

    chore(build): remove unneeded build inputs

--- nix/agent.nix
+++ nix/agent.nix
@@ -1,8 +1,6 @@
 {
   craneLib,
   stdenv,
-  qemu,
-  pkg-config,
 }: let
   src = craneLib.cleanCargoSource (craneLib.path ./..);
 
@@ -17,16 +15,8 @@
 in
   craneLib.buildPackage (commonArgs
     // {
-      depsBuildBuild = [
-        qemu
-      ];
-      nativeBuildInputs = [
-        pkg-config
-      ];
       inherit cargoArtifacts;
       inherit (craneLib.crateNameFromCargoToml {cargoToml = ../agent/Cargo.toml;}) pname;
       cargoExtraArgs = "--target aarch64-unknown-linux-gnu --bin grow-agent";
       CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
-      CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
-      HOST_CC = "${stdenv.cc.nativePrefix}cc";
     })
