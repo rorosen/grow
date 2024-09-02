072d913ab1bc99bcc50666ae900863a4c48f1082
Robert Rose <robert.rose@mailbox.org>
2024-09-02 22:12:14 +0200

    dashboard: update grow dashboard

--- agent/migrations/20240825201909_water-level-measurements.sql
+++ agent/migrations/20240825201909_water-level-measurements.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS water_level_measurements
     id            INTEGER PRIMARY KEY NOT NULL,
     measure_time  INTEGER             NOT NULL,
     label         TEXT                NOT NULL,
-    distance      REAL                NOT NULL
+    distance      INTEGER             NOT NULL
 );
