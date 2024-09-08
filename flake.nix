74810a0ce98215a86384d810c9dd4dce4ee63c30
Robert Rose <robert.rose@mailbox.org>
2024-09-08 02:23:48 +0200

    build: use flake utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     crane = {
       url = "github:ipetkov/crane";
@@ -18,54 +19,39 @@
   outputs =
     {
       nixpkgs,
+      flake-utils,
       crane,
       rust-overlay,
       ...
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        localSystem = system;
-        crossSystem = "aarch64-linux";
-        overlays = [ (import rust-overlay) ];
-      };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          localSystem = system;
+          crossSystem = "aarch64-linux";
+          overlays = [ (import rust-overlay) ];
+        };
+        mkCrossToolchain =
+          pkgs:
+          pkgs.pkgsBuildHost.rust-bin.stable.latest.default.override {
+            targets = [ "aarch64-unknown-linux-gnu" ];
+          };
 
-      crossToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default.override {
-        targets = [ "aarch64-unknown-linux-gnu" ];
-      };
-
-      craneLib = (crane.mkLib pkgs).overrideToolchain (_p: crossToolchain);
-      crates = pkgs.callPackage ./nix/packages/crates.nix { inherit craneLib; };
-      yesoreyeram-infinity-datasource =
-        pkgs.callPackage ./nix/packages/yesoreyeram-infinity-datasource.nix
-          { };
-    in
-    {
-      packages.${system} = {
-        inherit yesoreyeram-infinity-datasource;
-        inherit (crates)
-          agent
-          server
-          gpiotest
-          sensortest
-          ;
-      };
-
+        mkCraneLib = pkgs: (crane.mkLib pkgs).overrideToolchain (_p: (mkCrossToolchain pkgs));
+        crates = pkgs.callPackage ./nix/packages/crates.nix { craneLib = mkCraneLib pkgs; };
+        yesoreyeram-infinity-datasource =
+          pkgs.callPackage ./nix/packages/yesoreyeram-infinity-datasource.nix
+            { };
+      in
+      {
+        packages = {
+          inherit (crates) agent server sensortest;
+          inherit yesoreyeram-infinity-datasource;
+        };
+      }
+    )
+    // {
       nixosModules = import ./nix/modules;
-
-      overlays.default = _final: _prev: {
-        grow-agent = crates.agent;
-        grow-server = crates.server;
-        grow-gpiotest = crates.gpiotest;
-        grow-sensortest = crates.sensortest;
-        grafanaPlugins.yesoreyeram-infinity-datasource = yesoreyeram-infinity-datasource;
-      };
-
-      # devShells.${system}.default = craneLib.devShell {
-      #   packages = with pkgs; [
-      #     pkg-config
-      #     sqlx-cli
-      #   ];
-      # };
     };
 }
